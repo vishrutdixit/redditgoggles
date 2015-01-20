477830bfa91793c561a7d4ab9dc69846c0b3c142
vishrutdixit <vishrutdixit@gmail.com>
2015-01-20 14:09:34 -0600

    Landing Page

--- redditgrabber.js
+++ redditgrabber.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
     $('.svg-container').hide().fadeIn(1500);
-    $('.welcome-text').hide().fadeIn(4000);
+    //	$('.welcome-text').hide().fadeIn(3000);
     
 });
+
+$(".redirect-focus").click(function(e){
+	e.preventDefault();
+	
+	$("#search-field").focus();
+});
+
+
 
 
 $(document).on("click", ".vid-summary", function(e) { 
@@ -103,6 +111,7 @@ $(document).on("click", ".vid-summary", function(e) {
 
         				NProgress.inc();
         			}
+        			$(".main-cont").css("background-color", "#1b1b1b");
         			NProgress.done();
         		
         	});
@@ -135,4 +144,144 @@ function toggleSidebar() {
   	$("#toggle-glyph").toggleClass("active");
 
 }
+
+
+
+$("#search-form").submit(function(){
+
+NProgress.start();
+
+var $sidebar = $('.sidebar');
+var $urls = $('#urls');
+//var $counter = $('#counter');
+
+var baseurl = 'http://www.reddit.com/r/';
+var input = $('#search-field').val(); 
+
+var thumbnailbase1 = 'http://img.youtube.com/vi/';
+var thumbnailbase2 = '/hqdefault.jpg';
+
+if(input === ''){
+  var url = 'http://www.reddit.com/' + input + '.json';
+}
+else {
+  var url = baseurl + input + '/.json';
+}
+
+console.log(url);
+
+
+
+$.ajax({
+        type: 'GET',
+        url: url,
+        success: function(data){
+          //console.log(data);
+          $sidebar.empty();
+          $.each(data.data.children, function(i, child){
+              
+                //variables needed
+                var url = child.data.url; 
+                var suburl; 
+                var index;
+                var thumbnailurl; 
+                var index2; //used for second round to filter out extraneous stuff after the video ID
+                
+
+                
+                if(child.data.domain === 'youtube.com'){
+                    index = url.indexOf('=');
+                    index++; 
+                    suburl = url.substring(index); 
+                    //console.log(suburl); 
+
+                    
+                    if(suburl.indexOf('?') >= 0) {
+                      index2 = suburl.indexOf('?');
+                      suburl = suburl.substring(0, index2);
+                    }
+                    else if(suburl.indexOf('&') >= 0){
+                      index2 = suburl.indexOf('&')
+                      suburl = suburl.substring(0, index2);
+                    }
+                    else if(suburl.indexOf('#') >= 0){
+                      index2 = suburl.indexOf('#')
+                      suburl = suburl.substring(0, index2);
+                    }
+
+                    
+                    
+                    
+                    thumbnailurl = thumbnailbase1 + suburl + thumbnailbase2;
+                    //console.log(thumbnailurl);
+                    //console.log("-------")
+                    
+                    $sidebar.append('<div class="row video-item"> <div class="col-xs-12"> <a class="video-link" href="#"> <div class="vid-summary row"><div class="preview-holder col-md-4"><span class="helper"> </span> <img class="preview" src="' + thumbnailurl + '" height="75" width="100"/></div><div class="title-holder col-md-8"><p class="vid-title">' + child.data.title +'</p></div></div></a> </div> </div>');
+                    
+                    
+                    
+                }
+
+                else if(child.data.domain === 'youtu.be'){
+                  
+                  index = url.indexOf('be/');
+                  index+=3; 
+                  suburl = url.substring(index); 
+                  //console.log(suburl); 
+
+                  if(suburl.indexOf('?') >= 0) {
+                      index2 = suburl.indexOf('?');
+                      suburl = suburl.substring(0, index2);
+                    }
+                    else if(suburl.indexOf('&') >= 0){
+                      index2 = suburl.indexOf('&')
+                      suburl = suburl.substring(0, index2);
+                    }
+                    else if(suburl.indexOf('#') >= 0){
+                      index2 = suburl.indexOf('#')
+                      suburl = suburl.substring(0, index2);
+                    }
+                  
+                  thumbnailurl = thumbnailbase1 + suburl + thumbnailbase2;
+                  //console.log(thumbnailurl);
+                  //console.log("-------")
+                    
+                    $sidebar.append('<div class="row video-item"> <div class="col-xs-12"> <a class="video-link" href="#"> <div class="vid-summary row"><div class="preview-holder col-md-4"><span class="helper"> </span> <img class="preview" src="' + thumbnailurl + '" height="75" width="100"/></div><div class="title-holder col-md-8"><p class="vid-title">' + child.data.title +'</p></div></div></a> </div> </div>');
+                  
+                    
+                } 
+
+                NProgress.inc();
+                
+            });
+            
+            
+            if ($( ".sidebar" ).is( ".hide" ) ) {
+            
+            toggleSidebar();
+            }
+            $('.landing-content').hide();
+            $('.row.video-embed').removeClass('hide');
+
+            NProgress.done();
+
+            
+              
+              //counter+=25;
+              //if(data.data.after === null) isMore = false; 
+            
+        }
+      
+      });
+      return false;
+
+});
+
+
+
+
+  
+
+
+
 
